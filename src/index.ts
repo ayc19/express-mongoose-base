848f971c7abdf24c68e1e69c98dfca74074c6391
Alex Chan <Alex.Y.Chan.19@dartmouth.edu>
2023-02-06 13:15:44 -0800

    json body and db connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 import { router as usersRouter } from './routes/usersRouter';
+import { connectToServer } from './db'
+
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+connectToServer();
 
 app.get('/', (_req, res) => {
     res.send('hello there');
