eeac0bffc61ac434339322732562e133d31c09fe
Alex Chan <Alex.Y.Chan.19@dartmouth.edu>
2023-02-14 16:17:56 -0800

    mv entity type to model file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 
-import { router as usersRouter } from './routes/usersRouter';
 import { connectToServer } from './db'
 
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 connectToServer();
 
 app.get('/', (_req, res) => {
     res.send('hello there');
 });
-
-app.use('/users', usersRouter);
 
 app.listen(4000, () => {
     console.log('listening on port 4000');
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,10 +1,11 @@
 import { Schema, model } from 'mongoose';
 
-const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-    },
+export interface User {
+    name: string;
+}
+
+const UserSchema = new Schema<User>({
+    name: { type: String, required: true },
 });
 
-export const UserModel = model('User', UserSchema);
+export const UserModel = model<User>('User', UserSchema);
